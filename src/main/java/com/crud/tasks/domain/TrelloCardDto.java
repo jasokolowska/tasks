10f611be223542b006edcf2eb016715e055823aa
Joanna Sokolowska <joanna_s@op.pl>
2021-12-08 15:26:48 +0100

    config update

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,5 +1,7 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloCardDto {
 
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("desc")
     private String description;
+    @JsonProperty("pos")
     private String pos;
+    @JsonProperty("idList")
     private String listId;
 }

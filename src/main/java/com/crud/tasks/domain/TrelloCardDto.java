ba1879f0996e798859c26a1036526dac1d18b43a
Joanna Sokolowska <joanna_s@op.pl>
2021-12-08 19:48:16 +0100

    config update

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -9,10 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloCardDto {
 
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("desc")
     private String description;
+    @JsonProperty("pos")
     private String pos;
+    @JsonProperty("idList")
     private String listId;
 }

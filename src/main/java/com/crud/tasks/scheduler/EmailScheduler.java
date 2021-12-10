2ad7082703c218abf92f67100d8fb6f0b38f0f67
Joanna Sokolowska <joanna_s@op.pl>
2021-12-10 20:26:48 +0100

    23.4 Problem to solve

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -21,11 +21,13 @@ public class EmailScheduler {
     @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
+        String task = (size == 1)? "task" : "tasks";
+
         simpleEmailService.send(
                 new Mail(
                         adminConfig.getAdminMail(),
                         SUBJECT,
-                        "Currently in database you got: " + size + " tasks",
+                        "Currently in database you got: " + size + task,
                         null
                 )
         );

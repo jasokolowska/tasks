6c4ed5228c3dd07de3d190ac910500c6274747c4
Joanna Sokolowska <joanna_s@op.pl>
2022-01-30 14:18:04 +0100

    29.1 Facade Pattern add TaskMapperTestSuite

--- src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -1,4 +1,47 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class TaskMapperTest {
-  
+
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @Test
+    void testMapToTask() {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "first task", "empty");
+
+        //When
+        Task task = taskMapper.mapToTask(taskDto);
+
+        //Then
+        assertEquals("first task", task.getTitle());
+    }
+
+    @Test
+    void testMapToTaskDtoList() {
+        //Given
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1L, "task 1", "description"));
+        tasks.add(new Task(2L, "task 2", "description"));
+        tasks.add(new Task(3L, "task 3", "description"));
+
+        //When
+        List<TaskDto> taskDtos = taskMapper.mapToTaskDtoList(tasks);
+
+        //Then
+        assertEquals(3, taskDtos.size());
+        assertEquals("task 2", taskDtos.get(1).getTitle());
+    }
 }
--- src/test/java/com/crud/tasks/service/DbServiceTest.java
+++ src/test/java/com/crud/tasks/service/DbServiceTest.java
@@ -1,4 +1,58 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class DbServiceTest {
-  
+
+    @Autowired
+    private DbService dbService;
+
+    @Test
+    void testGetAllTasks() {
+        //Given
+        Task task1 = new Task(1L, "task 1", "content 1");
+        Task task2 = new Task(2L, "task 2", "content 2");
+        Task task3 = new Task(3L, "task 3", "content 3");
+        dbService.saveTask(task1);
+        dbService.saveTask(task2);
+        dbService.saveTask(task3);
+
+        //When
+        List<Task> tasksSaved = dbService.getAllTasks();
+
+        //Then
+        assertEquals(3, tasksSaved.size());
+
+        //CleanUp
+        dbService.deleteTask(task1.getId());
+        dbService.deleteTask(task2.getId());
+        dbService.deleteTask(task3.getId());
+    }
+
+    @Test
+    void getTask() {
+        //Given
+        Task task1 = new Task(1L, "task 1", "content 1");
+        dbService.saveTask(task1);
+
+        //When
+        Optional<Task> result = dbService.getTask(task1.getId());
+
+        //Then
+        assertEquals("task 1", result.get().getTitle());
+
+        //CleanUp
+        dbService.deleteTask(task1.getId());
+
+    }
+
 }

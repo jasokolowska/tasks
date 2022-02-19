db39fbeb35d2195cf4841f944d4a65a7f0c2b702
Joanna Sokolowska <joanna_s@op.pl>
2022-02-19 10:57:40 +0100

    Correct scheduler

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -18,8 +18,7 @@ public class EmailScheduler {
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
 
-//    @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String task = (size == 1)? "task" : "tasks";
